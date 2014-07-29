86296610f8206ed237fa0b9eb11913316925263e
yoriyuki <yoriyuki.y@gmail.com>
2014-07-29 21:30:49 +0900

    Age

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ uproplib
 ========
 
 Unicode Property Database
+
+Copyright and license
+---------------------
+
+uproplib is distributed under the terms of the GNU Lesser General Public
+License version 3.0 with OCaml linking exception, except 
+
+- the files under ucd whose condition of use and distribution are
+articulated by http://www.unicode.org/terms_of_use.html
--- tools/ucdCompiler.ml
+++ tools/ucdCompiler.ml
@@ -1,3 +1,5 @@
+open UCoreLib
+
 let get_ucd inf = try 
   let ic = if inf = "-" then stdin else open_in inf in
   let d = Uucd.decoder (`Channel ic) in 
@@ -9,8 +11,54 @@ let get_ucd inf = try
     exit 1
 with Sys_error e -> Printf.eprintf "%s\n%!" e; exit 1
 
-let () = Printf.printf "....."; flush_all ()
+let () = Printf.printf "Reading....."; flush_all ()
 
 let ucd = get_ucd "ucd/ucd.all.grouped.xml"
 
+let () = Printf.printf " done.\n"; flush_all ()
+
+(* age *)
+
+let () = Printf.printf "Processing age....."; flush_all ()
+
+type age = [ `Unassigned | `Version of int * int ]
+
+let map = ref (UMap.empty ((=) : age -> age -> bool))
+
+let () = for i = 0 to 0x10ffff do
+  if not (Uucd.is_scalar_value i) then () else begin
+    match Uucd.cp_prop ucd i Uucd.age with
+      Some p -> 
+	map := UMap.add (UChar.of_int_exn i) p !map
+    | None -> ()
+  end
+done
+
+module PHash = struct
+  type t = age
+  let equal : age -> age -> bool = (=)
+  let hash = Hashtbl.hash
+end
+
+module PTbl = UCharTbl.Make(PHash)
+
+let tbl = PTbl.of_map `Unassigned !map
+
+let tbl_mar = Marshal.to_string (tbl : PTbl.t) []
+(*let map_mar = Marshal.to_string !map []*)
+
+let age_mli_file = open_out "props/uPropAge.mli"
+
+let () = output_string age_mli_file "val age : UCoreLib.UChar.uchar -> [ `Unassigned | `Version of int * int ]\n"
+
+let () = flush_all ()
+let () = close_out age_mli_file
+
+let age_ml_file = open_out "props/uPropAge.ml"
+
+let () = output_string age_ml_file ("let tbl = Marshal.from_string \"" ^ (String.escaped tbl_mar) ^ "\" 0 \n")
+let () = output_string age_ml_file "let age u = UCoreLib.UCharTbl.get tbl u\n"
+
+let () = flush_all ()
+let () = close_out age_ml_file
 let () = Printf.printf " done.\n"; flush_all ()
