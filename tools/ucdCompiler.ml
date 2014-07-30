6341de5d54a2db49822525a0a932b10bf04e8cc6
yoriyuki <yoriyuki.y@gmail.com>
2014-07-31 00:21:59 +0900

    Functor version

--- tools/ucdCompiler.ml
+++ tools/ucdCompiler.ml
@@ -17,50 +17,75 @@ let ucd = get_ucd "ucd/ucd.all.grouped.xml"
 
 let () = Printf.printf " done.\n"; flush_all ()
 
-(* age *)
+module type PropSig = sig
+  type t
 
-let () = Printf.printf "Processing age....."; flush_all ()
+  val property : t Uucd.prop
+  val default : t
 
-type age = [ `Unassigned | `Version of int * int ]
-
-let map = ref (UMap.empty ((=) : age -> age -> bool))
-
-let () = for i = 0 to 0x10ffff do
-  if not (Uucd.is_scalar_value i) then () else begin
-    match Uucd.cp_prop ucd i Uucd.age with
-      Some p -> 
-	map := UMap.add (UChar.of_int_exn i) p !map
-    | None -> ()
-  end
-done
-
-module PHash = struct
-  type t = age
-  let equal : age -> age -> bool = (=)
-  let hash = Hashtbl.hash
+  val name : string
+  val type_name : string
 end
 
-module PTbl = UCharTbl.Make(PHash)
+module ProcProp (P : PropSig) = struct
+  module PHash = struct
+    type t = P.t
+    let equal :  P.t -> P.t -> bool = (=)
+    let hash = Hashtbl.hash
+  end
 
-let tbl = PTbl.of_map `Unassigned !map
+  module PTbl = UCharTbl.Make(PHash)
 
-let tbl_mar = Marshal.to_string (tbl : PTbl.t) []
+  let run () =
+    let () = (Printf.printf "Processing %s....." P.name); flush_all () in
+
+    let map = ref (UMap.empty ((=) : P.t -> P.t -> bool)) in
+	
+    let () = for i = 0 to 0x10ffff do
+      if not (Uucd.is_scalar_value i) then () else begin
+	match Uucd.cp_prop ucd i P.property with
+	  Some p -> 
+	    map := UMap.add (UChar.of_int_exn i) p !map
+	| None -> ()
+      end
+    done in
+
+    let tbl = PTbl.of_map P.default !map in
+
+    let tbl_mar = Marshal.to_string (tbl : PTbl.t) [] in
 (*let map_mar = Marshal.to_string !map []*)
 
-let age_mli_file = open_out "props/uPropAge.mli"
+    let age_mli_file = open_out ("props/uProp" ^ (String.capitalize P.name) ^ ".mli") in
 
-let () = output_string age_mli_file "type t = [ `Unassigned | `Version of int * int ]\n"
-let () = output_string age_mli_file "val get : UCoreLib.UChar.uchar -> [ `Unassigned | `Version of int * int ]\n"
+    let () = output_string age_mli_file ("type t = " ^ P.type_name ^ "\n") in
+    let () = output_string age_mli_file ("val get : UCoreLib.UChar.uchar -> " ^ P.type_name  ^ "\n") in
 
-let () = flush_all ()
-let () = close_out age_mli_file
+    let () = flush_all () in
+    let () = close_out age_mli_file in
 
-let age_ml_file = open_out "props/uPropAge.ml"
+    let age_ml_file = open_out "props/uPropAge.ml" in
 
-let () = output_string age_ml_file "type t = [ `Unassigned | `Version of int * int ]\n"
-let () = output_string age_ml_file ("let tbl = Marshal.from_string \"" ^ (String.escaped tbl_mar) ^ "\" 0 \n")
-let () = output_string age_ml_file "let get u = UCoreLib.UCharTbl.get tbl u\n"
+    let () = output_string age_ml_file ("type t = " ^ P.type_name ^ "\n") in
+    let () = output_string age_ml_file ("let tbl = Marshal.from_string \"" ^ (String.escaped tbl_mar) ^ "\" 0 \n") in
+    let () = output_string age_ml_file "let get u = UCoreLib.UCharTbl.get tbl u\n" in
 
-let () = flush_all ()
-let () = close_out age_ml_file
-let () = Printf.printf " done.\n"; flush_all ()
+    let () = flush_all () in
+    let () = close_out age_ml_file in
+    let () = Printf.printf " done.\n"; flush_all () in
+    ()
+end
+
+(* age *)
+
+module AgeSig = struct 
+  type t = [ `Unassigned | `Version of int * int ]
+
+  let property = Uucd.age
+  let default = `Unassigned
+  let name = "age"
+  let type_name = "[ `Unassigned | `Version of int * int ]"
+end
+
+module Age = ProcProp(AgeSig)
+
+let () = Age.run ()
