d5c63e472d47c1fda50fed4402b361ca58006468
yoriyuki <yoriyuki.y@gmail.com>
2014-08-12 22:56:06 +0900

    Dash

--- tools/ucdCompiler.ml
+++ tools/ucdCompiler.ml
@@ -112,7 +112,7 @@ module ProcBoolProp (P : BoolPropSig) = struct
     let prop_ml_file = open_out ("props/uProp" ^ P.name ^ ".ml") in
 
     let () = output_string prop_ml_file ("let tbl = Marshal.from_string \"" ^ (String.escaped tbl_mar) ^ "\" 0 \n") in
-    let () = output_string prop_ml_file "let get u = UCoreLib.UCharTbl.get tbl u\n" in
+    let () = output_string prop_ml_file "let get u = UCoreLib.UCharTbl.Bool.get tbl u\n" in
 
     let () = flush_all () in
     let () = close_out prop_ml_file in
@@ -210,3 +210,717 @@ end
 module BidiClass = ProcProp(BidiClassStr)
 
 let () = BidiClass.run ()
+
+(* Bidi Control *)
+
+module BidiControlStr = struct
+  let property = Uucd.bidi_control
+  let name = "BidiControl"
+end
+
+module BidiControl = ProcBoolProp(BidiControlStr)
+let () = BidiControl.run ()
+
+(* Bidi Mirrored *)
+
+module BidiMirroredStr = struct
+  let property = Uucd.bidi_mirrored
+  let name = "BidiMirrored"
+end
+
+module BidiMirrored = ProcBoolProp(BidiMirroredStr)
+let () = BidiMirrored.run ()
+
+(* Bidi Mirroring Glyph *)
+
+module BidiMirroringGlyphStr = struct 
+  type t = int option
+
+  let property = Uucd.bidi_mirroring_glyph
+  let default = None
+  let name = "BidiMirroringGlyph"
+  let type_name = "UCoreLib.UChar.t option"
+end
+
+module BidiMirroringGlyph = ProcProp(BidiMirroringGlyphStr)
+
+let () = BidiMirroringGlyph.run ()
+
+(* bidi_paired_bracket *) 
+
+module BidiPairedBracketStr = struct 
+  type t = [ `Cp of int | `Self ]
+
+  let property = Uucd.bidi_paired_bracket
+  let default = `Self
+  let name = "BidiPairedBracket"
+  let type_name = "[ `Cp of int | `Self ]"
+end
+
+module BidiPairedBracket = ProcProp(BidiPairedBracketStr)
+
+let () = BidiPairedBracket.run ()
+
+(* bidi_paired_bracket_type *)
+
+module BidiPairedBracketTypeStr = struct 
+  type t = [ `C | `N | `O ]
+
+  let property = Uucd.bidi_paired_bracket_type
+  let default = `N
+  let name = "BidiPairedBracketType"
+  let type_name = "[ `C | `N | `O ]"
+end
+
+module BidiPairedBracketType = ProcProp(BidiPairedBracketTypeStr)
+
+let () = BidiPairedBracketType.run ()
+
+(* block *)
+
+module BlockStr = struct 
+  type t =  [ `ASCII
+       | `Aegean_Numbers
+       | `Alchemical
+       | `Alphabetic_PF
+       | `Ancient_Greek_Music
+       | `Ancient_Greek_Numbers
+       | `Ancient_Symbols
+       | `Arabic
+       | `Arabic_Ext_A
+       | `Arabic_Math
+       | `Arabic_PF_A
+       | `Arabic_PF_B
+       | `Arabic_Sup
+       | `Armenian
+       | `Arrows
+       | `Avestan
+       | `Balinese
+       | `Bamum
+       | `Bamum_Sup
+       | `Bassa_Vah
+       | `Batak
+       | `Bengali
+       | `Block_Elements
+       | `Bopomofo
+       | `Bopomofo_Ext
+       | `Box_Drawing
+       | `Brahmi
+       | `Braille
+       | `Buginese
+       | `Buhid
+       | `Byzantine_Music
+       | `CJK
+       | `CJK_Compat
+       | `CJK_Compat_Forms
+       | `CJK_Compat_Ideographs
+       | `CJK_Compat_Ideographs_Sup
+       | `CJK_Ext_A
+       | `CJK_Ext_B
+       | `CJK_Ext_C
+       | `CJK_Ext_D
+       | `CJK_Radicals_Sup
+       | `CJK_Strokes
+       | `CJK_Symbols
+       | `Carian
+       | `Caucasian_Albanian
+       | `Chakma
+       | `Cham
+       | `Cherokee
+       | `Compat_Jamo
+       | `Control_Pictures
+       | `Coptic
+       | `Coptic_Epact_Numbers
+       | `Counting_Rod
+       | `Cuneiform
+       | `Cuneiform_Numbers
+       | `Currency_Symbols
+       | `Cypriot_Syllabary
+       | `Cyrillic
+       | `Cyrillic_Ext_A
+       | `Cyrillic_Ext_B
+       | `Cyrillic_Sup
+       | `Deseret
+       | `Devanagari
+       | `Devanagari_Ext
+       | `Diacriticals
+       | `Diacriticals_Ext
+       | `Diacriticals_For_Symbols
+       | `Diacriticals_Sup
+       | `Dingbats
+       | `Domino
+       | `Duployan
+       | `Egyptian_Hieroglyphs
+       | `Elbasan
+       | `Emoticons
+       | `Enclosed_Alphanum
+       | `Enclosed_Alphanum_Sup
+       | `Enclosed_CJK
+       | `Enclosed_Ideographic_Sup
+       | `Ethiopic
+       | `Ethiopic_Ext
+       | `Ethiopic_Ext_A
+       | `Ethiopic_Sup
+       | `Geometric_Shapes
+       | `Geometric_Shapes_Ext
+       | `Georgian
+       | `Georgian_Sup
+       | `Glagolitic
+       | `Gothic
+       | `Grantha
+       | `Greek
+       | `Greek_Ext
+       | `Gujarati
+       | `Gurmukhi
+       | `Half_And_Full_Forms
+       | `Half_Marks
+       | `Hangul
+       | `Hanunoo
+       | `Hebrew
+       | `High_PU_Surrogates
+       | `High_Surrogates
+       | `Hiragana
+       | `IDC
+       | `IPA_Ext
+       | `Imperial_Aramaic
+       | `Indic_Number_Forms
+       | `Inscriptional_Pahlavi
+       | `Inscriptional_Parthian
+       | `Jamo
+       | `Jamo_Ext_A
+       | `Jamo_Ext_B
+       | `Javanese
+       | `Kaithi
+       | `Kana_Sup
+       | `Kanbun
+       | `Kangxi
+       | `Kannada
+       | `Katakana
+       | `Katakana_Ext
+       | `Kayah_Li
+       | `Kharoshthi
+       | `Khmer
+       | `Khmer_Symbols
+       | `Khojki
+       | `Khudawadi
+       | `Lao
+       | `Latin_1_Sup
+       | `Latin_Ext_A
+       | `Latin_Ext_Additional
+       | `Latin_Ext_B
+       | `Latin_Ext_C
+       | `Latin_Ext_D
+       | `Latin_Ext_E
+       | `Lepcha
+       | `Letterlike_Symbols
+       | `Limbu
+       | `Linear_A
+       | `Linear_B_Ideograms
+       | `Linear_B_Syllabary
+       | `Lisu
+       | `Low_Surrogates
+       | `Lycian
+       | `Lydian
+       | `Mahajani
+       | `Mahjong
+       | `Malayalam
+       | `Mandaic
+       | `Manichaean
+       | `Math_Alphanum
+       | `Math_Operators
+       | `Meetei_Mayek
+       | `Meetei_Mayek_Ext
+       | `Mende_Kikakui
+       | `Meroitic_Cursive
+       | `Meroitic_Hieroglyphs
+       | `Miao
+       | `Misc_Arrows
+       | `Misc_Math_Symbols_A
+       | `Misc_Math_Symbols_B
+       | `Misc_Pictographs
+       | `Misc_Symbols
+       | `Misc_Technical
+       | `Modi
+       | `Modifier_Letters
+       | `Modifier_Tone_Letters
+       | `Mongolian
+       | `Mro
+       | `Music
+       | `Myanmar
+       | `Myanmar_Ext_A
+       | `Myanmar_Ext_B
+       | `NB
+       | `NKo
+       | `Nabataean
+       | `New_Tai_Lue
+       | `Number_Forms
+       | `OCR
+       | `Ogham
+       | `Ol_Chiki
+       | `Old_Italic
+       | `Old_North_Arabian
+       | `Old_Permic
+       | `Old_Persian
+       | `Old_South_Arabian
+       | `Old_Turkic
+       | `Oriya
+       | `Ornamental_Dingbats
+       | `Osmanya
+       | `PUA
+       | `Pahawh_Hmong
+       | `Palmyrene
+       | `Pau_Cin_Hau
+       | `Phags_Pa
+       | `Phaistos
+       | `Phoenician
+       | `Phonetic_Ext
+       | `Phonetic_Ext_Sup
+       | `Playing_Cards
+       | `Psalter_Pahlavi
+       | `Punctuation
+       | `Rejang
+       | `Rumi
+       | `Runic
+       | `Samaritan
+       | `Saurashtra
+       | `Sharada
+       | `Shavian
+       | `Shorthand_Format_Controls
+       | `Siddham
+       | `Sinhala
+       | `Sinhala_Archaic_Numbers
+       | `Small_Forms
+       | `Sora_Sompeng
+       | `Specials
+       | `Sundanese
+       | `Sundanese_Sup
+       | `Sup_Arrows_A
+       | `Sup_Arrows_B
+       | `Sup_Arrows_C
+       | `Sup_Math_Operators
+       | `Sup_PUA_A
+       | `Sup_PUA_B
+       | `Sup_Punctuation
+       | `Super_And_Sub
+       | `Syloti_Nagri
+       | `Syriac
+       | `Tagalog
+       | `Tagbanwa
+       | `Tags
+       | `Tai_Le
+       | `Tai_Tham
+       | `Tai_Viet
+       | `Tai_Xuan_Jing
+       | `Takri
+       | `Tamil
+       | `Telugu
+       | `Thaana
+       | `Thai
+       | `Tibetan
+       | `Tifinagh
+       | `Tirhuta
+       | `Transport_And_Map
+       | `UCAS
+       | `UCAS_Ext
+       | `Ugaritic
+       | `VS
+       | `VS_Sup
+       | `Vai
+       | `Vedic_Ext
+       | `Vertical_Forms
+       | `Warang_Citi
+       | `Yi_Radicals
+       | `Yi_Syllables
+       | `Yijing ] 
+
+  let property = Uucd.block
+  let default = `NB
+  let name = "Block"
+  let type_name = " [ `ASCII
+       | `Aegean_Numbers
+       | `Alchemical
+       | `Alphabetic_PF
+       | `Ancient_Greek_Music
+       | `Ancient_Greek_Numbers
+       | `Ancient_Symbols
+       | `Arabic
+       | `Arabic_Ext_A
+       | `Arabic_Math
+       | `Arabic_PF_A
+       | `Arabic_PF_B
+       | `Arabic_Sup
+       | `Armenian
+       | `Arrows
+       | `Avestan
+       | `Balinese
+       | `Bamum
+       | `Bamum_Sup
+       | `Bassa_Vah
+       | `Batak
+       | `Bengali
+       | `Block_Elements
+       | `Bopomofo
+       | `Bopomofo_Ext
+       | `Box_Drawing
+       | `Brahmi
+       | `Braille
+       | `Buginese
+       | `Buhid
+       | `Byzantine_Music
+       | `CJK
+       | `CJK_Compat
+       | `CJK_Compat_Forms
+       | `CJK_Compat_Ideographs
+       | `CJK_Compat_Ideographs_Sup
+       | `CJK_Ext_A
+       | `CJK_Ext_B
+       | `CJK_Ext_C
+       | `CJK_Ext_D
+       | `CJK_Radicals_Sup
+       | `CJK_Strokes
+       | `CJK_Symbols
+       | `Carian
+       | `Caucasian_Albanian
+       | `Chakma
+       | `Cham
+       | `Cherokee
+       | `Compat_Jamo
+       | `Control_Pictures
+       | `Coptic
+       | `Coptic_Epact_Numbers
+       | `Counting_Rod
+       | `Cuneiform
+       | `Cuneiform_Numbers
+       | `Currency_Symbols
+       | `Cypriot_Syllabary
+       | `Cyrillic
+       | `Cyrillic_Ext_A
+       | `Cyrillic_Ext_B
+       | `Cyrillic_Sup
+       | `Deseret
+       | `Devanagari
+       | `Devanagari_Ext
+       | `Diacriticals
+       | `Diacriticals_Ext
+       | `Diacriticals_For_Symbols
+       | `Diacriticals_Sup
+       | `Dingbats
+       | `Domino
+       | `Duployan
+       | `Egyptian_Hieroglyphs
+       | `Elbasan
+       | `Emoticons
+       | `Enclosed_Alphanum
+       | `Enclosed_Alphanum_Sup
+       | `Enclosed_CJK
+       | `Enclosed_Ideographic_Sup
+       | `Ethiopic
+       | `Ethiopic_Ext
+       | `Ethiopic_Ext_A
+       | `Ethiopic_Sup
+       | `Geometric_Shapes
+       | `Geometric_Shapes_Ext
+       | `Georgian
+       | `Georgian_Sup
+       | `Glagolitic
+       | `Gothic
+       | `Grantha
+       | `Greek
+       | `Greek_Ext
+       | `Gujarati
+       | `Gurmukhi
+       | `Half_And_Full_Forms
+       | `Half_Marks
+       | `Hangul
+       | `Hanunoo
+       | `Hebrew
+       | `High_PU_Surrogates
+       | `High_Surrogates
+       | `Hiragana
+       | `IDC
+       | `IPA_Ext
+       | `Imperial_Aramaic
+       | `Indic_Number_Forms
+       | `Inscriptional_Pahlavi
+       | `Inscriptional_Parthian
+       | `Jamo
+       | `Jamo_Ext_A
+       | `Jamo_Ext_B
+       | `Javanese
+       | `Kaithi
+       | `Kana_Sup
+       | `Kanbun
+       | `Kangxi
+       | `Kannada
+       | `Katakana
+       | `Katakana_Ext
+       | `Kayah_Li
+       | `Kharoshthi
+       | `Khmer
+       | `Khmer_Symbols
+       | `Khojki
+       | `Khudawadi
+       | `Lao
+       | `Latin_1_Sup
+       | `Latin_Ext_A
+       | `Latin_Ext_Additional
+       | `Latin_Ext_B
+       | `Latin_Ext_C
+       | `Latin_Ext_D
+       | `Latin_Ext_E
+       | `Lepcha
+       | `Letterlike_Symbols
+       | `Limbu
+       | `Linear_A
+       | `Linear_B_Ideograms
+       | `Linear_B_Syllabary
+       | `Lisu
+       | `Low_Surrogates
+       | `Lycian
+       | `Lydian
+       | `Mahajani
+       | `Mahjong
+       | `Malayalam
+       | `Mandaic
+       | `Manichaean
+       | `Math_Alphanum
+       | `Math_Operators
+       | `Meetei_Mayek
+       | `Meetei_Mayek_Ext
+       | `Mende_Kikakui
+       | `Meroitic_Cursive
+       | `Meroitic_Hieroglyphs
+       | `Miao
+       | `Misc_Arrows
+       | `Misc_Math_Symbols_A
+       | `Misc_Math_Symbols_B
+       | `Misc_Pictographs
+       | `Misc_Symbols
+       | `Misc_Technical
+       | `Modi
+       | `Modifier_Letters
+       | `Modifier_Tone_Letters
+       | `Mongolian
+       | `Mro
+       | `Music
+       | `Myanmar
+       | `Myanmar_Ext_A
+       | `Myanmar_Ext_B
+       | `NB
+       | `NKo
+       | `Nabataean
+       | `New_Tai_Lue
+       | `Number_Forms
+       | `OCR
+       | `Ogham
+       | `Ol_Chiki
+       | `Old_Italic
+       | `Old_North_Arabian
+       | `Old_Permic
+       | `Old_Persian
+       | `Old_South_Arabian
+       | `Old_Turkic
+       | `Oriya
+       | `Ornamental_Dingbats
+       | `Osmanya
+       | `PUA
+       | `Pahawh_Hmong
+       | `Palmyrene
+       | `Pau_Cin_Hau
+       | `Phags_Pa
+       | `Phaistos
+       | `Phoenician
+       | `Phonetic_Ext
+       | `Phonetic_Ext_Sup
+       | `Playing_Cards
+       | `Psalter_Pahlavi
+       | `Punctuation
+       | `Rejang
+       | `Rumi
+       | `Runic
+       | `Samaritan
+       | `Saurashtra
+       | `Sharada
+       | `Shavian
+       | `Shorthand_Format_Controls
+       | `Siddham
+       | `Sinhala
+       | `Sinhala_Archaic_Numbers
+       | `Small_Forms
+       | `Sora_Sompeng
+       | `Specials
+       | `Sundanese
+       | `Sundanese_Sup
+       | `Sup_Arrows_A
+       | `Sup_Arrows_B
+       | `Sup_Arrows_C
+       | `Sup_Math_Operators
+       | `Sup_PUA_A
+       | `Sup_PUA_B
+       | `Sup_Punctuation
+       | `Super_And_Sub
+       | `Syloti_Nagri
+       | `Syriac
+       | `Tagalog
+       | `Tagbanwa
+       | `Tags
+       | `Tai_Le
+       | `Tai_Tham
+       | `Tai_Viet
+       | `Tai_Xuan_Jing
+       | `Takri
+       | `Tamil
+       | `Telugu
+       | `Thaana
+       | `Thai
+       | `Tibetan
+       | `Tifinagh
+       | `Tirhuta
+       | `Transport_And_Map
+       | `UCAS
+       | `UCAS_Ext
+       | `Ugaritic
+       | `VS
+       | `VS_Sup
+       | `Vai
+       | `Vedic_Ext
+       | `Vertical_Forms
+       | `Warang_Citi
+       | `Yi_Radicals
+       | `Yi_Syllables
+       | `Yijing ]"
+end
+
+module Block = ProcProp(BlockStr)
+
+let () = Block.run ()
+
+(* canonical_combining_class  *)
+
+module CanonicalCombiningClassStr = struct 
+  type t = int
+
+  let property = Uucd.canonical_combining_class 
+  let default = 0
+  let name = "CanonicalCombiningClass"
+  let type_name = "int"
+end
+
+module CanonicalCombiningClass  = ProcProp(CanonicalCombiningClassStr)
+
+let () = CanonicalCombiningClass.run ()
+
+(* Bidi Mirrored *)
+
+module CasedStr = struct
+  let property = Uucd.cased
+  let name = "Cased"
+end
+
+module Cased = ProcBoolProp(CasedStr)
+let () = Cased.run ()
+
+(* case_folding *)
+
+module CaseFoldingStr = struct 
+  type t = [ `Cps of int list | `Self ]
+
+  let property = Uucd.case_folding 
+  let default = `Self
+  let name = "CaseFolding "
+  let type_name = "[`Cps of int list | `Self]"
+end
+
+module CaseFolding = ProcProp(CaseFoldingStr)
+
+let () = CaseFolding.run ()
+
+(* case_ignorable *)
+
+module CaseIgnorableStr = struct
+  let property = Uucd.case_ignorable
+  let name = "CaseIgnorable"
+end
+
+module CaseIgnorable = ProcBoolProp(CaseIgnorableStr)
+let () = CaseIgnorable.run ()
+
+(* changes_when_casefolded *)
+
+module ChangesWhenCasefoldedStr = struct
+  let property = Uucd.changes_when_casefolded
+  let name = "ChangesWhenCasefolded"
+end
+
+module ChangesWhenCasefolded = ProcBoolProp(ChangesWhenCasefoldedStr)
+let () = CaseIgnorable.run ()
+
+(* changes_when_casemapped *)
+
+module ChangesWhenCasemappedStr = struct
+  let property = Uucd.changes_when_casemapped
+  let name = "ChangesWhenCasemapped"
+end
+
+module ChangesWhenCasemapped = ProcBoolProp(ChangesWhenCasemappedStr)
+let () = ChangesWhenCasemapped.run ()
+
+(* changes_when_lowercased *)
+
+module ChangesWhenLowercasedStr = struct
+  let property = Uucd.changes_when_lowercased
+  let name = "ChangesWhenLowercased"
+end
+
+module ChangesWhenLowercased = ProcBoolProp(ChangesWhenLowercasedStr)
+let () = ChangesWhenLowercased.run ()
+
+(* changes_when_nfkc_casefolded *)
+
+module ChangesWhenNfkcCasefoldedStr = struct
+  let property = Uucd.changes_when_nfkc_casefolded
+  let name = "ChangesWhenNfkcCasefolded"
+end
+
+module ChangesWhenNfkcCasefolded = ProcBoolProp(ChangesWhenNfkcCasefoldedStr)
+let () = ChangesWhenNfkcCasefolded.run ()
+
+(* changes_when_titlecased *)
+
+module ChangesWhenTitlecasedStr = struct
+  let property = Uucd.changes_when_titlecased
+  let name = "ChangesWhenTitlecased"
+end
+
+module ChangesWhenTitlecased = ProcBoolProp(ChangesWhenTitlecasedStr)
+let () = ChangesWhenTitlecased.run ()
+
+(* changes_when_uppercased *)
+
+module ChangesWhenUppercasedStr = struct
+  let property = Uucd.changes_when_uppercased
+  let name = "ChangesWhenUppercased"
+end
+
+module ChangesWhenUppercased = ProcBoolProp(ChangesWhenUppercasedStr)
+let () = ChangesWhenUppercased.run ()
+
+(* composition_exclusion *)
+
+module CompositionExclusionStr = struct
+  let property = Uucd.composition_exclusion
+  let name = "CompositionExclusion"
+end
+
+module CompositionExclusion = ProcBoolProp(CompositionExclusionStr)
+let () = CompositionExclusion.run ()
+
+(* dash *)
+
+module DashStr = struct
+  let property = Uucd.dash
+  let name = "Dash"
+end
+
+module Dash = ProcBoolProp(DashStr)
+let () = Dash.run ()
